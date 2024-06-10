f875119050044f425a0915bc3d6a891da3da7bc8
Mrcode93 <alhlaqalhlaq667@gmail.com>
2024-06-10 15:35:41 +0300

    updated-amer

--- controllers/category_controller.go
+++ controllers/category_controller.go
@@ -3,6 +3,9 @@ package controllers
 import (
 	"ecommercebackend/models"
 	"ecommercebackend/repository"
+	"net/http"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,19 +19,19 @@ func GetCategories(c *gin.Context) {
 }
 
 func CreateCategory(c *gin.Context) {
-	var category models.Category
-	if err := c.ShouldBindJSON(&category); err != nil {
-		c.JSON(400, gin.H{"error": "Invalid request payload"})
+	var newCategory models.Category
+	if err := c.ShouldBindJSON(&newCategory); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request payload"})
 		return
 	}
-	id, err := repository.CreateCategory(category)
+	categoryID, err := repository.CreateCategory(newCategory)
 	if err != nil {
-		if err.Error() == "UNIQUE constraint failed: categories.name" {
-			c.JSON(400, gin.H{"error": "Category already exists, Try another name"})
+		if strings.Contains(err.Error(), "UNIQUE constraint failed: categories.name") {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Category already exists, Try another name"})
 			return
 		}
-		c.JSON(500, gin.H{"error": "Failed to create category"})
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create category"})
 		return
 	}
-	c.JSON(201, gin.H{"id": id})
+	c.JSON(http.StatusCreated, gin.H{"message": "Category created successfully", "id": categoryID})
 }
--- main/main.go
+++ main/main.go
@@ -3,14 +3,26 @@ package main
 import (
 	"ecommercebackend/db"
 	"ecommercebackend/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"time"
 )
 
 func main() {
-
 	gin.SetMode(gin.ReleaseMode)
 	db.InitDB()
 	server := gin.Default()
+
+	// Apply the CORS middleware
+	server.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
 	routes.Setup(server)
 	err := server.Run(":8080")
 	if err != nil {
