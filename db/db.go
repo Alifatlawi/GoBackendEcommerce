f6a3405ab6579d7306a85f31e60a1d42b81c0b5b
Ali Al-Fatlawi <alawi_amer@yahoo.co.uk>
2024-06-12 02:00:17 +0300

    database added

--- db/db.go
+++ db/db.go
@@ -2,15 +2,35 @@ package db
 
 import (
 	"database/sql"
-	_ "github.com/mattn/go-sqlite3"
+	"fmt"
+	"github.com/joho/godotenv"
+	_ "github.com/microsoft/go-mssqldb"
 	"log"
+	"os"
+	"strconv"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-	var err error
-	DB, err = sql.Open("sqlite3", "main.db")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	server := os.Getenv("AZURE_SQL_SERVER_NAME")
+	user := os.Getenv("AZURE_SQL_DATABASE_USER")
+	password := os.Getenv("AZURE_SQL_DATABASE_PASSWORD")
+	port, err := strconv.Atoi(os.Getenv("AZURE_SQL_SERVER_PORT"))
+	if err != nil {
+		log.Fatal("Error converting port to integer:", err)
+	}
+	database := os.Getenv("AZURE_SQL_DATABASE_NAME")
+
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+
+	DB, err = sql.Open("sqlserver", connString)
 	if err != nil {
 		log.Fatal("Could not connect to database:", err)
 	}
@@ -21,22 +41,27 @@ func InitDB() {
 }
 
 func createTables() {
+	// Check if categories table exists and create if it does not
 	createCategoriesTable := `
-    CREATE TABLE IF NOT EXISTS categories (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT NOT NULL UNIQUE
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='categories' AND xtype='U')
+    CREATE TABLE categories (
+        id INT PRIMARY KEY IDENTITY(1,1),
+        name NVARCHAR(100) NOT NULL UNIQUE
     );`
 
+	// Check if products table exists and create if it does not
 	createProductsTable := `
-CREATE TABLE IF NOT EXISTS products (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    description TEXT NOT NULL,
-    img_url TEXT NOT NULL,
-    price REAL NOT NULL,
-    category_id INTEGER,
-    FOREIGN KEY (category_id) REFERENCES categories(id)
-);`
+    IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='products' AND xtype='U')
+    CREATE TABLE products (
+        id INT PRIMARY KEY IDENTITY(1,1),
+        name NVARCHAR(100) NOT NULL,
+        description NVARCHAR(255) NOT NULL,
+        img_url NVARCHAR(255) NOT NULL,
+        price FLOAT NOT NULL,
+        category_id INT,
+        FOREIGN KEY (category_id) REFERENCES categories(id)
+    );`
+
 	_, err := DB.Exec(createCategoriesTable)
 	if err != nil {
 		log.Fatal("Could not create categories table:", err)
--- go.mod
+++ go.mod
@@ -8,7 +8,7 @@ require (
 	github.com/gin-contrib/cors v1.7.2
 	github.com/gin-gonic/gin v1.10.0
 	github.com/joho/godotenv v1.5.1
-	github.com/mattn/go-sqlite3 v1.14.22
+	github.com/microsoft/go-mssqldb v1.7.2
 )
 
 require (
@@ -23,6 +23,8 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.20.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
